ae32f3d2697ee11da0780c5d3a4c40a9c8423d1f
Naren-1405 <narendarsr79@gmail.com>
2026-07-19 18:29:00 +0530

    added secure registration with bcrypt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,30 +1,56 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const registerUser = async (req, res) => {
-    const {
-        name,
-        email,
-        phone,
-        password,
-        role,
-        location
-    } = req.body;
+    try {
+        const {
+            name,
+            email,
+            phone,
+            password,
+            role,
+            location
+        } = req.body;
+        if (!name || !email || !phone || !password || !role || !location) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
 
-    const user = await User.create({
-        name,
-        email,
-        phone,
-        password,
-        role,
-        location
-    });
+        const existsuser = await User.findOne({ email });
 
-    res.status(201).json({
-        success: true,
-        message: "User registered successfully",
-        user
-    });
-};
+        if (existsuser) {
+            return res.status(400).json({
+                success: false,
+                message: "Email already exists"
+            });
+        }
+    
+        const hashpass = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            phone,
+            password: hashpass,
+            role,
+            location
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            user
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
 module.exports = {
     registerUser
 };
